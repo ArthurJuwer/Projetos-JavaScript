4906291588e93610ab468638f60682251c2e8feb
ArthurJuwer <arthur.juwer99@gmail.com>
2024-04-26 23:37:58 -0300

    Atualização do projeto, deixando ele mais dinamico, limpo e eficaz

--- Termo/script.js
+++ Termo/script.js
@@ -1,10 +1,14 @@
 var container = document.getElementsByClassName("word")
 
 var level = 0
-var palavras = ["luiza".toUpperCase()] // aumentar lista 
+var contador = 0
+var palavras = ["luiza", "maria", "lucas", "paulo", "juwer", "areia", "terra"] 
 var letras = []
 var result = "DERROTA"
 
+var min = 0; max = palavras.length
+var random = Math.floor(Math.random() * (max - min) - min)
+console.log(random)
 
 document.addEventListener("keydown", (evento)=>{
     
@@ -12,19 +16,17 @@ document.addEventListener("keydown", (evento)=>{
 
     if (evento.key === 'Enter') {  
         
-        for(let i = 0; i < 5; i++){            
-            letras = palavras[0][i]
-            // trocar o zero por random
-            console.log(level)
+        for(let i = 0; i < input.length; i++){            
+            letras = palavras[random][i]
 
-            if(input[i].value.toUpperCase() == letras){
+            if(input[i].value == letras){
                 input[i].classList.add("correct")
                 
             }  
             else{
-                for(let x = 0; x < 5; x++){
+                for(let x = 0; x < input.length; x++){
                     
-                    if(input[x].value.toUpperCase() == letras){
+                    if(input[x].value == letras){
                         input[x].classList.add("contains")
                     }
                     else{
@@ -38,32 +40,32 @@ document.addEventListener("keydown", (evento)=>{
                     }
                 }                
             }
-            
         }          
 
         if(container[level+1].classList.contains("off") == true){
-
-            if (input[0].classList.contains("correct") && 
-            input[1].classList.contains("correct") && 
-            input[2].classList.contains("correct") &&
-            input[3].classList.contains("correct") &&
-            input[4].classList.contains("correct") ){
-                result = "VITORIA"
-                alert(result)
+                        
+            for(let i = 0; i < input.length; i++){
+                    if(input[i].classList.contains("correct")){
+                        contador++
+                    }
+                }
+            if(contador == input.length){
+            result = "VITORIA"
+            alert(result)
             }
+            
             else {
+                contador = 0
                 container[level].classList.add("off")
                 container[level+1].classList.remove("off")
-
                 level++
             }
         }
-        if(level == 5){
+        if(level == container.length-1){
+            // REMOVER GAMBIARRA E TIRAR O -1
             alert(result)
         }
-        
-    }              
-    
+    }         
 })
 
 
