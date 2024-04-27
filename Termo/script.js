8d39279066a2615918ecf23232e5d82e81333feb
ArthurJuwer <arthur.juwer99@gmail.com>
2024-04-27 10:43:43 -0300

    Ultimas Atualizações do projeto

--- Termo/script.js
+++ Termo/script.js
@@ -4,68 +4,78 @@ var level = 0
 var contador = 0
 var palavras = ["luiza", "maria", "lucas", "paulo", "juwer", "areia", "terra"] 
 var letras = []
-var result = "DERROTA"
+
+var textResult = document.querySelector(".result p")
 
 var min = 0; max = palavras.length
 var random = Math.floor(Math.random() * (max - min) - min)
 console.log(random)
 
+function mostrarResult(){
+
+    var result = document.querySelector(".result")
+    var all = document.querySelector(".all")
+
+    result.classList.remove("dn")
+    all.classList.add("off")
+}
+
 document.addEventListener("keydown", (evento)=>{
     
     var input = container[level].getElementsByTagName("input")
 
-    if (evento.key === 'Enter') {  
-        
-        for(let i = 0; i < input.length; i++){            
-            letras = palavras[random][i]
+    if(textResult.innerHTML == "DERROTA"){
+        if (evento.key === 'Enter') {  
+            
+            for(let i = 0; i < input.length; i++){            
+                letras = palavras[random][i]
 
-            if(input[i].value == letras){
-                input[i].classList.add("correct")
-                
-            }  
-            else{
-                for(let x = 0; x < input.length; x++){
+                if(input[i].value == letras){
+                    input[i].classList.add("correct")
                     
-                    if(input[x].value == letras){
-                        input[x].classList.add("contains")
-                    }
-                    else{
-                        if(!(input[i].value == "")){
-                            input[i].classList.add("incorrect")
+                }  
+                else{
+                    for(let x = 0; x < input.length; x++){
+                        
+                        if(input[x].value == letras){
+                            input[x].classList.add("contains")
                         }
                         else{
-                            input[i].classList.remove("incorrect")
+                            if(!(input[i].value == "")){
+                                input[i].classList.add("incorrect")
+                            }
+                            else{
+                                input[i].classList.remove("incorrect")
+                            }
+                            
                         }
-                        
-                    }
-                }                
-            }
-        }          
-
-        if(container[level+1].classList.contains("off") == true){
-                        
-            for(let i = 0; i < input.length; i++){
-                    if(input[i].classList.contains("correct")){
-                        contador++
-                    }
+                    }                
                 }
-            if(contador == input.length){
-            result = "VITORIA"
-            alert(result)
+            }          
+
+            if(container[level+1].classList.contains("off") == true){
+                            
+                for(let i = 0; i < input.length; i++){
+                        if(input[i].classList.contains("correct")){
+                            contador++
+                        }
+                    }
+                if(contador == input.length){
+                textResult.innerHTML = "VITÓRIA"
+                mostrarResult()
+                }
+                else {
+                    contador = 0
+                    container[level].classList.add("off")
+                    container[level+1].classList.remove("off")
+                    level++
+                }
             }
-            
-            else {
-                contador = 0
-                container[level].classList.add("off")
-                container[level+1].classList.remove("off")
-                level++
+            if(level == container.length-1){
+                mostrarResult()
             }
-        }
-        if(level == container.length-1){
-            // REMOVER GAMBIARRA E TIRAR O -1
-            alert(result)
-        }
-    }         
+        }  
+    }       
 })
 
 
